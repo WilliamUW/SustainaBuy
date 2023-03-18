1592def00a57174b8ab3b569a90a1154d989f217
William Wang <cs.william.wang@gmail.com>
2023-03-18 14:22:00 -0400

    Working extension

---
+++ getPageMetas.js
@@ -0,0 +1,16 @@
+var metas = document.getElementsByTagName('meta'); 
+var metaArr = [];
+for (var i=0; i<metas.length; i++) { 
+	var name = metas[i].getAttribute("name");
+	var property = metas[i].getAttribute("property");
+	var httpequiv = metas[i].getAttribute("http-equiv");
+	var content = metas[i].getAttribute("content");
+	var charset = metas[i].getAttribute("charset");
+	
+	metaArr.push([name, property, httpequiv, content, charset]);
+} 
+
+chrome.runtime.sendMessage({
+	method:"getMetas",
+	metas:metaArr
+});
---
+++ popup.js
@@ -0,0 +1,22 @@
+function getMetas() {
+	var message = document.querySelector('#metaTable');
+	chrome.tabs.executeScript(null, {
+		file: "getPageMetas.js"
+	}, function() {
+		// If you try it into an extensions page or the webstore/NTP you'll get an error
+		if (chrome.runtime.lastError) {
+			message.innerText = 'There was an error : \n' + chrome.runtime.lastError.message;
+		}
+	});
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+	var metaTable = document.getElementById('metaTable');
+	if (request.method == "getMetas") {
+		for (var i=0; i<request.metas.length; i++) { 
+			metaTable.innerHTML += "<tr><td>"+request.metas[i][0]+"</td><td>"+request.metas[i][1]+"</td><td>"+request.metas[i][2]+"</td><td>"+request.metas[i][3]+"</td><td>"+request.metas[i][4]+"</td></tr>"; 
+		} 
+	}
+});
+
+window.onload = getMetas;
